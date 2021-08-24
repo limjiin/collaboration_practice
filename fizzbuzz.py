820e442cd6aadd3d180f879c50fca59075eb73e3
Joripje <lawofjrj@gmail.com>
2021-08-24 16:59:39 +0900

    docs: Add comentary about fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,14 @@
 # fizzbuzz 1 to 30
 
+
 for i in range(1, 30 + 1):
+# 1부터 30까지 출력하는 반복문
     if i % 3 == 0 or i % 5 == 0:
         print('fizz' * (i % 3 == 0) + 'buzz' * (i % 5 == 0))
+# 3의 배수에는 fizz 출력, 5의 배수에는 buzz 출력, 15의 배수에는 fizzbuzz 출력
     else:
         print(i)
+# 그 외에는 숫자를 출력
 print('-' * 30)
 print('finish!')
+# 끝!
